a362d9d312af658e0e0b30a9894db01a32576da8
AhmedFarazi <71428466+AhmedFarazi@users.noreply.github.com>
2021-04-16 00:40:02 -0600

    Added code to delete employee

--- frontend/admin/deleteEmployee.php
+++ frontend/admin/deleteEmployee.php
@@ -1,5 +1,37 @@
 <?php 
 
 include 'admin.php'; 
+include '../../backend/database.php';
+include '../../logic/logic.php';
 
 ?>
+
+
+<!DOCTYPE HTML>
+<html>
+Delete an Employee:
+<head>
+  <title>Register Form</title>
+  <link rel="shortcut icon" href="#">
+</head>
+<body>
+  
+ <form action="deleteEmployeeLogic.php" method="POST">
+  <table>
+   <tr>
+    <td>Employee ID: </td>
+    <td>
+      <input type="number" name="employeeid" required>
+    </td>
+   </tr>
+   <tr>
+    <td><input type="submit" value="Submit" name="SubmitButton"></td>
+    </tr>
+
+  </table>
+</form>
+
+</body>
+</html>
+
+
